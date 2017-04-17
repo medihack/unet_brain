ecab5a530eff8840ebd2390a5fa35142abef4bae
Hagen Meredig <hagen@Hagens-iMac.fritz.box>
2017-04-17 17:01:18 +0200

    clean initial version

---
+++ model.py
@@ -0,0 +1,199 @@
+
+from __future__ import division
+from __future__ import print_function
+
+from keras import backend as K
+from keras.engine import Input, Model
+from keras.layers import Conv3D, MaxPooling3D, UpSampling3D, Activation
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers.merge import Concatenate
+from keras.layers.core import Activation
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+
+#todo: modify loss function...
+
+
+# dice-coef-loss
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + 1.) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1.)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+
+def get_unet_3d(vol_x, vol_y, vol_z, chn, optimizer=Adam(lr=0.00001), loss=dice_coef_loss,
+             metrics=[dice_coef]):
+
+    print('Data format: ' + K.image_data_format())
+
+    if K.image_data_format() == 'channels_first':
+        input_dims = (chn, vol_x, vol_y, vol_z)
+        feat_axis = 1
+    else:
+        input_dims = (vol_x, vol_y, vol_z, chn)
+        feat_axis = 4
+
+    # u-net model
+    inputs = Input(shape=input_dims)
+    conv1 = Conv3D(32, (3, 3, 3), activation=None, padding='same')(inputs) #Conv3D --> (filters, kernel_size, ...)
+    conv1 = BatchNormalization(axis=feat_axis, scale=False)(conv1)
+    conv1 = Activation('relu')(conv1)
+    conv1 = Conv3D(64, (3, 3, 3), activation=None, padding='same')(conv1)
+    conv1 = BatchNormalization(axis=feat_axis, scale=False)(conv1)
+    conv1 = Activation('relu')(conv1)
+
+    pool1 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv1)
+
+    conv2 = Conv3D(64, (3, 3, 3), activation=None, padding='same')(pool1)
+    conv2 = BatchNormalization(axis=feat_axis, scale=False)(conv2)
+    conv2 = Activation('relu')(conv2)
+    conv2 = Conv3D(128, (3, 3, 3), activation=None, padding='same')(conv2)
+    conv2 = BatchNormalization(axis=feat_axis, scale=False)(conv2)
+    conv2 = Activation('relu')(conv2)
+
+    pool2 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv2)
+
+    conv3 = Conv3D(128, (3, 3, 3), activation=None, padding='same')(pool2)
+    conv3 = BatchNormalization(axis=feat_axis, scale=False)(conv3)
+    conv3 = Activation('relu')(conv3)
+    conv3 = Conv3D(256, (3, 3, 3), activation=None, padding='same')(conv3)
+    conv3 = BatchNormalization(axis=feat_axis, scale=False)(conv3)
+    conv3 = Activation('relu')(conv3)
+
+    pool3 = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(conv3)
+
+    conv4 = Conv3D(256, (3, 3, 3), activation=None, padding='same')(pool3)
+    conv4 = BatchNormalization(axis=feat_axis, scale=False)(conv4)
+    conv4 = Activation('relu')(conv4)
+    conv4 = Conv3D(512, (3, 3, 3), activation=None, padding='same')(conv4)
+    conv4 = BatchNormalization(axis=feat_axis, scale=False)(conv4)
+    conv4 = Activation('relu')(conv4)
+
+    up1 = UpSampling3D(size=(2, 2, 2))(conv4)
+    up1 = Concatenate(axis=feat_axis)([conv3, up1])
+
+    upconv1 = Conv3D(256, (3, 3, 3), activation=None, padding='same')(up1)
+    upconv1 = BatchNormalization(axis=feat_axis, scale=False)(upconv1)
+    upconv1 = Activation('relu')(upconv1)
+    upconv1 = Conv3D(256, (3, 3, 3), activation=None, padding='same')(upconv1)
+    upconv1 = BatchNormalization(axis=feat_axis, scale=False)(upconv1)
+    upconv1 = Activation('relu')(upconv1)
+
+    up2 = UpSampling3D(size=(2, 2, 2))(upconv1)
+    up2 = Concatenate(axis=feat_axis)([conv2, up2])
+
+    upconv2 = Conv3D(128, (3, 3, 3), activation=None, padding='same')(up2)
+    upconv2 = BatchNormalization(axis=feat_axis, scale=False)(upconv2)
+    upconv2 = Activation('relu')(upconv2)
+    upconv2 = Conv3D(128, (3, 3, 3), activation=None, padding='same')(upconv2)
+    upconv2 = BatchNormalization(axis=feat_axis, scale=False)(upconv2)
+    upconv2 = Activation('relu')(upconv2)
+
+    up3 = UpSampling3D(size=(2, 2, 2))(upconv2)
+    up3 = Concatenate(axis=feat_axis)([conv1, up3])
+
+    upconv3 = Conv3D(64, (3, 3, 3), activation=None, padding='same')(up3)
+    upconv3 = BatchNormalization(axis=feat_axis, scale=False)(upconv3)
+    upconv3 = Activation('relu')(upconv3)
+    upconv3 = Conv3D(64, (3, 3, 3), activation=None, padding='same')(upconv3)
+    upconv3 = BatchNormalization(axis=feat_axis, scale=False)(upconv3)
+    upconv3 = Activation('relu')(upconv3)
+
+    conv_final = Conv3D(4, (3, 3, 3), activation='sigmoid', padding='same')(upconv3)
+
+    model = Model(inputs=inputs, outputs=conv_final)
+    model.summary()
+    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+
+    return model
+
+def get_unet_2d(sz_x, sz_y, chn, optimizer=Adam(lr=0.00001), loss=dice_coef_loss,
+             metrics=[dice_coef]):
+
+    print('Data format: ' + K.image_data_format())
+
+    if K.image_data_format() == 'channels_first':
+        input_dims = (chn, sz_x, sz_y)
+        feat_axis = 1
+    else:
+        input_dims = (sz_x, sz_y, chn)
+        feat_axis = 3
+
+    # u-net model
+    inputs = Input(shape=input_dims)
+    conv1 = Conv2D(32, (3, 3), activation=None, padding='same')(inputs)
+    conv1 = BatchNormalization(axis=feat_axis, scale=False)(conv1)
+    conv1 = Activation('relu')(conv1)
+    conv1 = Conv2D(64, (3, 3), activation=None, padding='same')(conv1)
+    conv1 = BatchNormalization(axis=feat_axis, scale=False)(conv1)
+    conv1 = Activation('relu')(conv1)
+
+    pool1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation=None, padding='same')(pool1)
+    conv2 = BatchNormalization(axis=feat_axis, scale=False)(conv2)
+    conv2 = Activation('relu')(conv2)
+    conv2 = Conv2(128, (3, 3), activation=None, padding='same')(conv2)
+    conv2 = BatchNormalization(axis=feat_axis, scale=False)(conv2)
+    conv2 = Activation('relu')(conv2)
+
+    pool2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation=None, padding='same')(pool2)
+    conv3 = BatchNormalization(axis=feat_axis, scale=False)(conv3)
+    conv3 = Activation('relu')(conv3)
+    conv3 = Conv2D(256, (3, 3), activation=None, padding='same')(conv3)
+    conv3 = BatchNormalization(axis=feat_axis, scale=False)(conv3)
+    conv3 = Activation('relu')(conv3)
+
+    pool3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation=None, padding='same')(pool3)
+    conv4 = BatchNormalization(axis=feat_axis, scale=False)(conv4)
+    conv4 = Activation('relu')(conv4)
+    conv4 = Conv2D(512, (3, 3), activation=None, padding='same')(conv4)
+    conv4 = BatchNormalization(axis=feat_axis, scale=False)(conv4)
+    conv4 = Activation('relu')(conv4)
+
+    up1 = UpSampling2D(size=(2, 2))(conv4)
+    up1 = Concatenate(axis=feat_axis)([conv3, up1])
+
+    upconv1 = Conv2D(256, (3, 3), activation=None, padding='same')(up1)
+    upconv1 = BatchNormalization(axis=feat_axis, scale=False)(upconv1)
+    upconv1 = Activation('relu')(upconv1)
+    upconv1 = Conv2D(256, (3, 3), activation=None, padding='same')(upconv1)
+    upconv1 = BatchNormalization(axis=feat_axis, scale=False)(upconv1)
+    upconv1 = Activation('relu')(upconv1)
+
+    up2 = UpSampling2D(size=(2, 2))(upconv1)
+    up2 = Concatenate(axis=feat_axis)([conv2, up2])
+
+    upconv2 = Conv2D(128, (3, 3), activation=None, padding='same')(up2)
+    upconv2 = BatchNormalization(axis=feat_axis, scale=False)(upconv2)
+    upconv2 = Activation('relu')(upconv2)
+    upconv2 = Conv2D(128, (3, 3), activation=None, padding='same')(upconv2)
+    upconv2 = BatchNormalization(axis=feat_axis, scale=False)(upconv2)
+    upconv2 = Activation('relu')(upconv2)
+
+    up3 = UpSampling2D(size=(2, 2))(upconv2)
+    up3 = Concatenate(axis=feat_axis)([conv1, up3])
+
+    upconv3 = Conv2D(64, (3, 3), activation=None, padding='same')(up3)
+    upconv3 = BatchNormalization(axis=feat_axis, scale=False)(upconv3)
+    upconv3 = Activation('relu')(upconv3)
+    upconv3 = Conv2D(64, (3, 3), activation=None, padding='same')(upconv3)
+    upconv3 = BatchNormalization(axis=feat_axis, scale=False)(upconv3)
+    upconv3 = Activation('relu')(upconv3)
+
+    conv_final = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(upconv3)
+
+    model = Model(inputs=inputs, outputs=conv_final)
+    model.summary()
+    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+
+    return model
---
+++ util.py
@@ -0,0 +1,201 @@
+from __future__ import division
+from __future__ import print_function
+import numpy as np
+import os
+from helpers import normalize_and_center
+from helpers import to_categorical
+from helpers import get_rand_z_chunk
+from helpers import rotation
+from helpers import shift
+from helpers import shear
+from helpers import zoom
+
+
+class VolSegDataManager(object):
+    """Class to manage volume and respective segmentation Data.
+    #Arguments:
+        vol_folder: Path to volume (and segmentation, see below) data.
+        seg_folder: Path to segmentation data. If not specified segmentation
+            files are supposed to be in the same folder as volume files.
+        vol_prefix: prefix for finding volume files. Files containing volume
+            data should begin with a uniform prefix to separate from
+            segmentation files.
+        seg_prefix: prefix for finding segmentation files.
+        test_fract: Fraction of the data used as test/validation data.
+            Default = 0.1.
+        verbose: Verbosity. 0: no printouts. 1: Print basic information
+            (default).
+        """
+    def __init__(self, vol_folder, seg_folder='', vol_prefix='',
+                 seg_prefix='', test_fract=0.1, verbose=1):
+        if seg_folder == '':
+            seg_folder = vol_folder
+        self.vol_folder = vol_folder
+        self.seg_folder = seg_folder
+        self.data_dict = {}
+        self.train_ind_list = []
+        self.test_ind_list = []
+        self.data_count = 0
+        self.data_aug = False
+        self.rotrg = 180 #range of random rotations when data_aug==True
+        self.shearrg = 0.05 #range of random shear when data_aug==True
+        self.zoomrg =  0.2 #range of random zoom when data_aug==True
+        self.shiftrg = 0.05 #range of random shift when data_aug==True
+        self.verbose = verbose
+        #get filepaths
+        for entry in os.listdir(self.vol_folder):
+            if entry[:len(vol_prefix)] == vol_prefix:
+                vol_filename = entry
+                seg_filename = vol_filename[len(vol_prefix):]
+                seg_filename = seg_prefix + seg_filename
+                assert seg_filename in os.listdir(self.seg_folder), 'segmentation file ' + seg_filename + ' for ' + vol_filename + ' doesnt exist.'
+                self.data_dict[self.data_count] = (vol_folder + '/' + vol_filename, seg_folder + '/' + seg_filename)
+                self.data_count += 1
+        assert (test_fract <= 1.) and (test_fract >= 0.), 'fraction of data used for testing mus be between 0 and 1.'
+        #split up in train data and test data
+        self.ntest = int(test_fract * self.data_count)
+        self.ntrain = self.data_count - self.ntest
+        # --> take ntest random test examples
+        for i in range(self.ntest):
+            index = int(np.random.random() * (self.data_count - 1) + 0.5)
+            self.test_ind_list.append(index)
+        # --> use rest as training data
+        for i in range(self.data_count):
+            if not (i in self.test_ind_list):
+                self.train_ind_list.append(i)
+        if self.verbose == 1:
+            print('Data Manager: got ' + str(self.data_count) + ' files')
+            print('Data Manager: using ' + str(self.ntrain) + ' files as training data')
+            print('Data Manager: using ' + str(self.ntest) + ' files as test/validation data')
+
+    def _load(self, filepath):
+        """internal load fuction."""
+        if filepath[-4:] == '.img':
+            import nibabel as nib
+            data = nib.load(filepath).get_data()
+        elif path[-4:] == '.npy':
+            data = np.load(filepath)
+        return data
+
+    def get_sample_i(self, index):
+        if self.verbose == 1:
+            print('Data Manager: getting sample from file with index ' + str(index) + '')
+        x_path, y_path = self.data_dict[index]
+        x = self._load(x_path)
+        x = normalize_and_center(x)
+        y = self._load(y_path)
+        if self.data_aug:
+            xrot = (np.random.random() * self.rotrg * 2.) - self.rotrg
+            yrot = (np.random.random() * self.rotrg * 2.) - self.rotrg
+            zrot = (np.random.random() * self.rotrg * 2.) - self.rotrg
+            xshear = (np.random.random() * self.shearrg * 2.) - self.shearrg
+            yshear = (np.random.random() * self.shearrg * 2.) - self.shearrg
+            zshear = (np.random.random() * self.shearrg * 2.) - self.shearrg
+            xshift = (np.random.random() * self.shiftrg * 2.) - self.shiftrg
+            yshift = (np.random.random() * self.shiftrg * 2.) - self.shiftrg
+            zshift = (np.random.random() * self.shiftrg * 2.) - self.shiftrg
+            zm = (np.random.random() * self.zoomrg * 2.) - self.zoomrg
+            x = zoom(x, 1. + zm)
+            y = zoom(y, 1. + zm)
+            x = rotation(x, xrot, yrot, zrot)
+            y = rotation(y, xrot, yrot, zrot)
+            x = shift(x, xshift, yshift, zshift)
+            y = shift(y, xshift, yshift, zshift)
+            x = shear(x, xshear, yshear, zshear)
+            y = shear(y, xshear, yshear, zshear)
+        y = to_categorical(y)
+        return x, y
+
+    def get_rand_sample(self):
+        ind = int(np.random.random() * (self.data_count - 1) + 0.5)
+        return self.get_sample_i(ind)
+
+    def get_rand_train_sample(self):
+        ind = int(np.random.random() * (self.ntrain - 1) + 0.5)
+        index = self.train_ind_list[ind]
+        return self.get_sample_i(index)
+
+    def get_rand_test_sample(self):
+        index = self.test_ind_list[int(np.random.random() * (self.ntest - 1) + 0.5)]
+        return self.get_sample_i(index)
+
+    def get_train_generator(self):
+        while True:
+            yield self.get_rand_train_sample()
+
+    def get_test_generator(self):
+        while True:
+            yield self.get_rand_test_sample()
+
+    def get_sample_generator(self):
+        while True:
+            yield self.get_rand_sample()
+
+    def get_rand_batch(self, batch_size=1):
+        x_batch = []
+        y_batch = []
+        for i in range(batch_size):
+            x, y = self.get_rand_sample()
+            x_batch.append(x)
+            y_batch.append(y)
+        return np.array(x_batch), np.array(y_batch)
+
+    def get_rand_train_batch(self, batch_size=1):
+        x_batch = []
+        y_batch = []
+        for i in range(batch_size):
+            x, y = self.get_rand_train_sample()
+            x_batch.append(x)
+            y_batch.append(y)
+        return np.array(x_batch), np.array(y_batch)
+
+    def get_rand_test_batch(self, batch_size=1):
+        x_batch = []
+        y_batch = []
+        for i in range(batch_size):
+            x, y = self.get_rand_test_sample()
+            x_batch.append(x)
+            y_batch.append(y)
+        return np.array(x_batch), np.array(y_batch)
+
+    def get_train_batch_generator_chunks(self, nchunks, overlap, batch_size=1):
+        def get_croped_batch():
+            batch = self.get_rand_train_batch(batch_size=batch_size)
+            new_scan_batch = []
+            new_seg_batch = []
+            for i in range(batch_size):
+                scan, seg = batch[0][i], batch[1][i]
+                scan, seg = get_rand_z_chunk((scan, seg), nchunks, overlap)
+                new_scan_batch.append(scan)
+                new_seg_batch.append(seg)
+                new_batch = np.array(new_scan_batch), np.array(new_seg_batch)
+            return new_batch
+        yield get_croped_batch()
+
+    def get_test_batch_generator_chunks(self, nchunks, overlap, batch_size=1):
+        def get_croped_batch():
+            batch = self.get_rand_test_batch(batch_size=batch_size)
+            new_scan_batch = []
+            new_seg_batch = []
+            for i in range(batch_size):
+                scan, seg = batch[0][i], batch[1][i]
+                scan, seg = get_rand_z_chunk((scan, seg), nchunks, overlap)
+                new_scan_batch.append(scan)
+                new_seg_batch.append(seg)
+                new_batch = np.array(new_scan_batch), np.array(new_seg_batch)
+            return new_batch
+        yield get_croped_batch()
+
+    def get_batch_generator(self, batch_size=1):
+        while True:
+            yield self.get_rand_batch(batch_size=batch_size)
+
+    def get_train_batch_generator(self, batch_size=1):
+        while True:
+            yield self.get_rand_train_batch(batch_size=batch_size)
+
+    def get_test_batch_generator(self, batch_size=1):
+        while True:
+            yield self.get_rand_test_batch(batch_size=batch_size)
+
+# todo: implement chunking as an option like in data_aug
